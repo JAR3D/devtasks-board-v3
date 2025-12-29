df898f3f6f0a81ddb02c93bb9371624227989379
Joao Rosado <joaorosado@Joaos-Air-2.home>
2025-12-29 18:10:47 +0000

    feat: add Task mongoose model with schema definitions

---
+++ lib/models/Task.ts
@@ -0,0 +1,31 @@
+import mongoose from 'mongoose';
+
+const taskSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: String,
+    status: {
+      type: String,
+      enum: ['BACKLOG', 'IN_PROGRESS', 'DONE'],
+      default: 'BACKLOG',
+    },
+    priority: {
+      type: String,
+      enum: ['LOW', 'MEDIUM', 'HIGH'],
+      default: 'MEDIUM',
+    },
+    tags: [String],
+  },
+  {
+    timestamps: true,
+  },
+);
+
+type TTaskDocument = mongoose.InferSchemaType<typeof taskSchema> & {
+  _id: string;
+};
+
+export const Task = mongoose.model<TTaskDocument>('Task', taskSchema);
