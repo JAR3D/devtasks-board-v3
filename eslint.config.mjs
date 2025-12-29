fa74c8f27a91e191164f213fe1438213432dd7f0
Joao Rosado <joaorosado@Joaos-Air-2.home>
2025-12-29 11:07:04 +0000

    chore: add linting config and deps

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,13 +1,52 @@
 import { defineConfig, globalIgnores } from "eslint/config";
+import globals from "globals";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
+import eslintConfigPrettier from "eslint-config-prettier";
+import eslintPluginPrettier from "eslint-plugin-prettier";
+import react from "eslint-plugin-react";
+import reactHooks from "eslint-plugin-react-hooks";
+import stylistic from "@stylistic/eslint-plugin";
+
+const prettierOptions = {
+  singleQuote: true,
+  semi: true,
+  tabWidth: 2,
+  endOfLine: "lf",
+};
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
-  // Override default ignores of eslint-config-next.
+  {
+    files: ["**/*.{ts,tsx}"],
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      globals: { ...globals.browser },
+      parserOptions: {
+        project: "./tsconfig.json",
+        tsconfigRootDir: import.meta.dirname,
+        ecmaFeatures: { jsx: true },
+      },
+    },
+    settings: { react: { version: "detect" } },
+    plugins: {
+      "@stylistic": stylistic,
+      prettier: eslintPluginPrettier,
+      "react-hooks": reactHooks,
+      react,
+    },
+    rules: {
+      ...react.configs.recommended.rules,
+      ...reactHooks.configs.recommended.rules,
+      "react/jsx-uses-react": "off",
+      "react/react-in-jsx-scope": "off",
+      "prettier/prettier": ["error", prettierOptions],
+    },
+  },
+  eslintConfigPrettier,
   globalIgnores([
-    // Default ignores of eslint-config-next:
     ".next/**",
     "out/**",
     "build/**",
