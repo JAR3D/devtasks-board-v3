108c04c1787fb7f1a69b710b4ffd516ea4d1dac4
Joao Rosado <joaorosado@Joaos-Air-2.home>
2025-12-29 18:27:41 +0000

    feat: define task DTO types and enums

---
+++ lib/types/taskTypes.ts
@@ -0,0 +1,13 @@
+export type TStatus = 'BACKLOG' | 'IN_PROGRESS' | 'DONE';
+export type TPriority = 'LOW' | 'MEDIUM' | 'HIGH';
+
+export interface ITaskDTO {
+  title: string;
+  description: string;
+  status: TStatus;
+  priority: TPriority;
+  tags: string[];
+  createdAt: string;
+  updatedAt: string;
+  _id: string;
+}
