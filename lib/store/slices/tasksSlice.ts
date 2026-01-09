5cd2bc2b0ee1386b77f9af1e13835aa41b1b7df0
Joao Rosado <joaorosado@Joaos-Air-2.home>
2026-01-09 08:24:57 +0000

    feat: add store tasksSlice

---
+++ lib/store/slices/tasksSlice.ts
@@ -0,0 +1,33 @@
+import { createSlice, type PayloadAction } from '@reduxjs/toolkit';
+
+import type { ITaskDTO } from '@/lib/types/taskTypes';
+
+type TTasksState = ITaskDTO[];
+
+const initialState: TTasksState = [];
+
+const tasksSlice = createSlice({
+  name: 'tasks',
+  initialState,
+  reducers: {
+    setTasks(state, action: PayloadAction<ITaskDTO[]>) {
+      return action.payload;
+    },
+    saveTask(state, action: PayloadAction<ITaskDTO>) {
+      const index = state.findIndex((task) => task._id === action.payload._id);
+
+      if (index >= 0) {
+        state[index] = action.payload;
+      } else {
+        state.unshift(action.payload);
+      }
+    },
+    removeTask(state, action: PayloadAction<string>) {
+      return state.filter((task) => task._id !== action.payload);
+    },
+  },
+});
+
+export const { setTasks, saveTask, removeTask } = tasksSlice.actions;
+
+export default tasksSlice.reducer;
